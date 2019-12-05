148698c24ee301c3824ea10d949ee5cab752b9ef
Tony Pulickal <ttp@hashicorp.com>
2019-12-05 16:08:20 -0500

    update example for 0.12

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 data "template_file" "vpc_template" {
-  template = "${file("${path.module}/template.yaml")}"
+  template = file("${path.module}/template.yaml")
 }
 
 resource "aws_cloudformation_stack" "vpc" {
   name = "${var.name}-${var.env}-vpc-stack"
-  parameters {
-    Active = "${var.active}"
-    AvailabilityZone1 = "${var.az1}"
-    AvailabilityZone2 = "${var.az2}"
-    AvailabilityZone3 = "${var.az3}"
-    EnableNat = "${var.enable_nat}"
-    EnvName = "${var.env}"
-    Name = "${var.name}"
+  parameters = {
+    Active            = var.active
+    AvailabilityZone1 = var.az1
+    AvailabilityZone2 = var.az2
+    AvailabilityZone3 = var.az3
+    EnableNat         = var.enable_nat
+    EnvName           = var.env
+    Name              = var.name
   }
-  template_body = "${data.template_file.vpc_template.rendered}"
-  on_failure = "DELETE"
+  template_body = data.template_file.vpc_template.rendered
+  on_failure    = "DELETE"
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,76 @@
 output "public_subnets" {
-  value = "${join(",", compact(split(",", "${aws_cloudformation_stack.vpc.outputs["PublicSubnet1"]},${aws_cloudformation_stack.vpc.outputs["PublicSubnet2"]},${aws_cloudformation_stack.vpc.outputs["PublicSubnet3"]}")))}"
+  value = join(
+    ",",
+    compact(
+      split(
+        ",",
+        "${aws_cloudformation_stack.vpc.outputs["PublicSubnet1"]},${aws_cloudformation_stack.vpc.outputs["PublicSubnet2"]},${aws_cloudformation_stack.vpc.outputs["PublicSubnet3"]}",
+      ),
+    ),
+  )
 }
 
 output "public_subnet_1" {
-  value = "${aws_cloudformation_stack.vpc.outputs["PublicSubnet1"]}"
+  value = aws_cloudformation_stack.vpc.outputs["PublicSubnet1"]
 }
 
 output "public_subnet_2" {
-  value = "${aws_cloudformation_stack.vpc.outputs["PublicSubnet2"]}"
+  value = aws_cloudformation_stack.vpc.outputs["PublicSubnet2"]
 }
 
 output "public_subnet_3" {
-  value = "${aws_cloudformation_stack.vpc.outputs["PublicSubnet3"]}"
+  value = aws_cloudformation_stack.vpc.outputs["PublicSubnet3"]
 }
 
 output "private_subnets" {
-  value = "${join(",", compact(split(",", "${aws_cloudformation_stack.vpc.outputs["PrivateSubnet1"]},${aws_cloudformation_stack.vpc.outputs["PrivateSubnet2"]},${aws_cloudformation_stack.vpc.outputs["PrivateSubnet3"]}")))}"
+  value = join(
+    ",",
+    compact(
+      split(
+        ",",
+        "${aws_cloudformation_stack.vpc.outputs["PrivateSubnet1"]},${aws_cloudformation_stack.vpc.outputs["PrivateSubnet2"]},${aws_cloudformation_stack.vpc.outputs["PrivateSubnet3"]}",
+      ),
+    ),
+  )
 }
 
 output "private_subnet_1" {
-  value = "${aws_cloudformation_stack.vpc.outputs["PrivateSubnet1"]}"
+  value = aws_cloudformation_stack.vpc.outputs["PrivateSubnet1"]
 }
 
 output "private_subnet_2" {
-  value = "${aws_cloudformation_stack.vpc.outputs["PrivateSubnet2"]}"
+  value = aws_cloudformation_stack.vpc.outputs["PrivateSubnet2"]
 }
 
 output "private_subnet_3" {
-  value = "${aws_cloudformation_stack.vpc.outputs["PrivateSubnet3"]}"
+  value = aws_cloudformation_stack.vpc.outputs["PrivateSubnet3"]
 }
 
 output "spare_subnet_1" {
-  value = "${aws_cloudformation_stack.vpc.outputs["SpareSubnet1"]}"
+  value = aws_cloudformation_stack.vpc.outputs["SpareSubnet1"]
 }
 
 output "spare_subnets" {
-  value = "${join(",", compact(split(",", "${aws_cloudformation_stack.vpc.outputs["SpareSubnet1"]},${aws_cloudformation_stack.vpc.outputs["SpareSubnet2"]},${aws_cloudformation_stack.vpc.outputs["SpareSubnet3"]}")))}"
+  value = join(
+    ",",
+    compact(
+      split(
+        ",",
+        "${aws_cloudformation_stack.vpc.outputs["SpareSubnet1"]},${aws_cloudformation_stack.vpc.outputs["SpareSubnet2"]},${aws_cloudformation_stack.vpc.outputs["SpareSubnet3"]}",
+      ),
+    ),
+  )
 }
 
 output "spare_subnet_2" {
-  value = "${aws_cloudformation_stack.vpc.outputs["SpareSubnet2"]}"
+  value = aws_cloudformation_stack.vpc.outputs["SpareSubnet2"]
 }
 
 output "spare_subnet_3" {
-  value = "${aws_cloudformation_stack.vpc.outputs["SpareSubnet3"]}"
+  value = aws_cloudformation_stack.vpc.outputs["SpareSubnet3"]
 }
 
 output "vpc_id" {
-  value = "${aws_cloudformation_stack.vpc.outputs["VpcId"]}"
+  value = aws_cloudformation_stack.vpc.outputs["VpcId"]
 }
+
